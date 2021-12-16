e8ed985cd253c83aafcd59f78ca898d14192dd9a
Anh-Tôn Duong Gheude <duonganhton@gmail.com>
2021-12-16 01:09:11 +0100

    add Username, Date for Blogpost

--- models/BlogPost.js
+++ models/BlogPost.js
@@ -2,7 +2,12 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema;
 const BlogPostSchema = new Schema({
 title: String,
-body: String
+body: String,
+username: String,
+datePosted:{ /* can declare property type with an object like this because we need 'default' */
+type: Date,
+default: new Date()
+}
 });
 
 const BlogPost = mongoose.model('BlogPost',BlogPostSchema);
