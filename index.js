5f292aaa6f330e7bd1386d002fc60cbb9b5550a5
Anh-Tôn Duong Gheude <duonganhton@gmail.com>
2021-12-23 22:22:25 +0100

    Refactor to MVC model

--- index.js
+++ index.js
@@ -1,64 +1,37 @@
 const express = require('express')
 const app = new express()
-const path = require('path')
 const ejs = require('ejs')
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser')
-const BlogPost = require('./models/BlogPost.js')
 const fileUpload = require('express-fileupload')
+const newPostController = require('./controllers/newPost')
+const homeController = require('./controllers/home')
+const storePostController = require('./controllers/storePost')
+const getPostController = require('./controllers/getPost')
+const validateMiddleware = require('./middleware/validationMiddleware');
 
-mongoose.connect('mongodb://localhost/my_database', {useNewUrlParser: true})
-const validateMiddleWare = (req,res,next)=>{
-    if(req.files == null || req.body.title == null || req.body.title == null){
-    return res.redirect('/posts/new')
-    }
-    next()
-}
-    
+mongoose.connect('mongodb://localhost/my_database', { useNewUrlParser: true })
+
+
 app.use(express.static('public'))
 app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({extended:true}))
+app.use(bodyParser.urlencoded({ extended: true }))
 app.use(fileUpload())
-app.use('/posts/store',validateMiddleWare)
-app.set('view engine','ejs')
-app.get('/', async (req,res)=>{
-    const blogposts = await BlogPost.find({})
-    console.log(blogposts)
-    res.render('index',{blogposts});
-})
-app.get('/about',(req,res)=>{
-    //res.sendFile(path.resolve(__dirname,'pages/about.html'))
-    res.render('about')
-})
-app.get('/contact',(req,res)=>{
-    //res.sendFile(path.resolve(__dirname,'pages/contact.html'))
-    res.render('contact')
-})
-app.get('/post',(req,res)=>{
-    //res.sendFile(path.resolve(__dirname,'pages/post.html'))
-    res.render('post')
-})
-app.get('/posts/new',(req,res)=>{
-    res.render('create')
-    })
-app.get('/post/:id',async (req,res)=>{
-        const blogpost = await BlogPost.findById(req.params.id)
-        res.render('post',{
-        blogpost
-        })
-})
+app.use('/posts/store', validateMiddleware)
+app.set('view engine', 'ejs')
 
 
-app.post('/posts/store', (req,res)=>{
-    let image = req.files.image;
-    image.mv(path.resolve(__dirname,'public/img',image.name),async (error)=>{
-    await BlogPost.create({ ...req.body, image: '/img/' + image.name})
-    res.redirect('/')
-    })
-})
-    
-        
-app.listen(4000, ()=>{
+app.get('/', homeController)
+
+app.get('/post/:id', getPostController)
+
+
+app.post('/posts/store', storePostController)
+
+
+app.get('/posts/new', newPostController)
+
+app.listen(4000, () => {
 
     console.log('App listening on port 4000')
 })
